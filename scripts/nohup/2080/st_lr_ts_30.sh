8b00926b685e5a8a876237cffb1ceb694dc4ed36
lwyanne <921174623@qq.com>
2021-04-01 18:18:12 +0800

    .

--- scripts/nohup/2080/st_lr_ts_30.sh
+++ scripts/nohup/2080/st_lr_ts_30.sh
@@ -5,11 +5,16 @@
 # python /home/shuying/CPAE/scripts/main_pretrain.py --ini-file /home/shuying/CPAE/logs/cpc/CPAELSTM44_AT/dim12_out12_lr_0.03_bs256_future_sgd_cos_t0.07.ini --save-every 100 &  
 # # 9425
 
-# python /home/shuying/CPAE/scripts/main_pretrain.py --ini-file /home/shuying/CPAE/logs/cpc/CPAELSTM44/st_lr_3e-4.ini --save-every 100 --resume True &
-# # 31582
+# python /home/shuying/CPAE/scripts/main_pretrain.py --ini-file /home/shuying/CPAE/logs/cpc/CPAELSTM44/dim12_out12_lr_0.0003_bs256_ts30_adam_cos_t5.ini --save-every 100 --resume False &
+# python /home/shuying/CPAE/scripts/main_pretrain.py --ini-file /home/shuying/CPAE/logs/cpc/CPAELSTM44/dim12_out12_lr_0.0003_bs256_ts30_adam_cos_t1.ini --save-every 100 --resume False &
+# python /home/shuying/CPAE/scripts/main_pretrain.py --ini-file /home/shuying/CPAE/logs/cpc/CPAELSTM44/dim12_out12_lr_0.0003_bs256_ts30_adam_cos_t0.1.ini --save-every 100 --resume False &
+# python /home/shuying/CPAE/scripts/main_pretrain.py --ini-file /home/shuying/CPAE/logs/cpc/CPAELSTM44/dim12_out12_lr_0.0003_bs256_ts30_adam_cos_t0.5.ini --save-every 100 --resume False &
+# # # 31582
 
 # python /home/shuying/CPAE/scripts/main_pretrain.py --ini-file /home/shuying/CPAE/logs/cpc/CPAELSTM44/dim12_out12_lr_0.0003_bs256_ts30_adam_cos_t0.07.ini --save-every 100 &  
 # #18526
 
 # python /home/shuying/CPAE/scripts/main_pretrain.py --ini-file /home/shuying/CPAE/logs/cpc/CPAELSTM44/dim12_out12_lr_0.0003_bs256_future_adam_cos_t0.07.ini --save-every 100 &  
-python /home/shuying/CPAE/scripts/main_pretrain.py --ini-file /home/shuying/CPAE/logs/cpc/CPAELSTM44/dim12_out12_lr_1_bs256_future_adam_cos_t0.07.ini --save-every 100 &  
+# python /home/shuying/CPAE/scripts/main_pretrain.py --ini-file /home/shuying/CPAE/logs/cpc/CPAELSTM44/st_lr_3e-4_future_cos_t0.07.ini --save-every 100 &  
+# python /home/shuying/CPAE/scripts/main_pretrain.py --ini-file /home/shuying/CPAE/logs/cpc/CPAELSTM44/st_lr_3e-4_ts30_cos_t0.5.ini --save-every 100 &  
+python /home/shuying/CPAE/scripts/main_pretrain.py --ini-file /home/shuying/CPAE/logs/cpc/CPAELSTM44/st_lr_3e-4.ini --save-every 100 &  
